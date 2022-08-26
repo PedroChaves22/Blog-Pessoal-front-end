7fd714a5861fbddaa652a9fc8a62567d05d39220
Pedro <pedrocsca@gmail.com>
2022-08-25 22:52:46 -0300

    criando Blog Pessoal com react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+        <h1 className='titulo'>Home</h1>
+        <img src='sp.jpg' className='img' alt='imagem da cidade de São Paulo'/>
+        </>
+    );
+}
+ 
+export default Home;

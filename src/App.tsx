0fb40b649028684dd0866efebb0fc4ce0af7f177
Pedro <pedrocsca@gmail.com>
2022-08-26 22:43:00 -0300

    atualizando e implementando UI no blog pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import Home from './paginas/home/Home'
 import './App.css';
-
+import Footer from './componentes/estaticos/footer/Footer';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import Home from './paginas/home/Home';
 
 function App() {
   return (
-   <Home/>
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
